7dc3b7d1660575e1100fb274e646ac8ba63b65e5
Xilophor <ryanzgreg@gmail.com>
2024-01-14 17:58:46 -0500

    Initial commit

---
+++ BestScoreBoards/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace BestScoreBoards
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger = base.Logger;
+            
+            Logger.LogInfo("BestScoreBoards has loaded!");
+        }
+
+        internal new static ManualLogSource Logger;
+    }
+}
